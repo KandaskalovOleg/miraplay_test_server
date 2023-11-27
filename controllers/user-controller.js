e99bde732b4c9ce779e26331b08800709bfe1932
Oleh Kandaskalov <olegkandaskalov@gmail.com>
2023-11-27 14:24:35 +0200

    update cors

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const userService = require('../services/user-service');
 const UserService = require('../services/user-service');
 const {validationResult, cookie} = require('express-validator');
