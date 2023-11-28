e9123cafc812b776830629858d1d91e6b448d523
Oleh Kandaskalov <olegkandaskalov@gmail.com>
2023-11-28 17:26:51 +0200

    add redirect link

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -47,7 +47,7 @@ class UserController {
     try {
       const activationLink = req.params.link;
       await userService.activate(activationLink);
-      return res.redirect(process.env.CLIENT_URL);
+      return res.redirect(`${process.env.CLIENT_URL}/miraplay_test_client/games`);
     } catch (e) {
       next(e);
     }
