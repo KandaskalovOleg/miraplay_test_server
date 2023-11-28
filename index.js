03dc269078a9c4e8efdb18ff1e153b1d4293ee97
Oleh Kandaskalov <olegkandaskalov@gmail.com>
2023-11-28 17:24:24 +0200

    add sameSite

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
   credentials: true,
-  origin: process.env.CLIENT_URL
+  origin: process.env.CLIENT_URL,
+  sameSite: 'none',
 }));
 app.use('/api', router);
 app.use(errorMiddleware);
